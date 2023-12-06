fa5f50b83d98d434411ac00d507bec5a3a36992c
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-06 17:45:13 -0300

    add twitter card

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,18 @@
+export function App () {
+    return(
+           <article>
+            <header>
+                <img alt="El avatar de Midudev" src="https://unavatar.io/twitter/midudev"/>
+                <div>
+                    <strong>Miguel Angel Duran</strong>
+                    <span>@midudev</span>
+                </div>
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+
+
+            </article>
+    )
+}
