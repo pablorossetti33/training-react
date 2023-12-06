142d8ef4ddda8de932f5dd753ebb69d7e2cf0343
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-06 18:13:09 -0300

    add style css

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,18 +1,12 @@
+import React from "react";
+import "./App.css"
+import { TwitterFollowCard } from "./TwitterFollowCard";
+
 export function App () {
     return(
-           <article>
-            <header>
-                <img alt="El avatar de Midudev" src="https://unavatar.io/twitter/midudev"/>
-                <div>
-                    <strong>Miguel Angel Duran</strong>
-                    <span>@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button>Seguir</button>
-            </aside>
-
-
-            </article>
+           <>
+           <TwitterFollowCard userName="midudev" name="Miguel Angel Duran"/>
+           <TwitterFollowCard userName="midudev" name="Miguel Angel Duran"/>
+           </>
     )
 }
