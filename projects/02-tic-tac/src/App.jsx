66e378c681f3803dec3a3da75df8d00976ce1148
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-10 19:32:45 -0300

    feat: add board and square

--- projects/02-tic-tac/src/App.jsx
+++ projects/02-tic-tac/src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
+
 const TURNS = {
   X: "x",
   O: "o"
 
 }
 
-const board = Array(9).fill(null)
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`
+  return(
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
 
    function App() {
+    const [board, setBoard] = useState(Array(9).fill(null))
+
+    const [turn,SetTurn] = useState(TURNS.X)
+
     return(
      <main className="board">
      <h1>Tic tac </h1>
@@ -14,14 +29,23 @@ const board = Array(9).fill(null)
       {
         board.map((_, index) => {
           return(
-            <div key={index}>
-              <span>
-                {index}
-              </span>
-            </div>
+            <Square
+            key = {index}
+            index={index}
+            >
+              {board[index]}
+              </Square>
           )
         })
       }
+     </section>
+     <section className="turn">
+       <Square isSelected={turn === TURNS.X}>
+        {TURNS.X}
+        </Square>
+       <Square isSelected={turn === TURNS.O}>
+        {TURNS.O}
+        </Square>
      </section>
      </main>
     )
