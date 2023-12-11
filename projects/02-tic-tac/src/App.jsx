f154d7b2b096bf61e62526366690a5867bda964b
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-11 11:24:28 -0300

    refactor: updateBoard & setWinner

--- projects/02-tic-tac/src/App.jsx
+++ projects/02-tic-tac/src/App.jsx
@@ -12,7 +12,7 @@ const Square = ({children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? "is-selected" : ""}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
 
   return(
@@ -21,15 +21,58 @@ const Square = ({children, isSelected, updateBoard, index}) => {
     </div>
   )
 }
+  const WINNER_COMBOS = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+]
+
+
 
    function App() {
     const [board, setBoard] = useState(Array(9).fill(null))
 
     const [turn,setTurn] = useState(TURNS.X)
+    //null es que no hay ganador, false hay empate.
+    const [winner, setWinner] = useState(null) 
 
-    const updateBoard = () =>{
+    const checkWinner = (boardToCheck) => {
+      //Revisamos las combinaciones
+      for (const combo of WINNER_COMBOS){
+        const [a, b, c] = combo
+        if (
+          boardToCheck[a] &&
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+        ){
+          return boardToCheck[a]
+        }
+      }
+      //Si no hay ganador:
+      return null
+    }
+
+    const updateBoard = (index) =>{
+      //No actualizamos esta posición si ya tiene algo.
+      if (board[index] || winner) return
+      //Actualizar tablero
+      const newBoard = [...board]
+      newBoard[index] = turn
+      setBoard(newBoard)
+      //Cambiar el turno
       const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
       setTurn(newTurn)
+      //Revisar si hay ganador
+      const newWinner = checkWinner(newBoard)
+      if (newWinner) {
+        alert(`El ganador es ${newWinner}`)
+        setWinner(newWinner)
+      }
     }
 
     return(
