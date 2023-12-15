06266216adeba6d150d220e7493c8d69ec3f8f99
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-15 18:44:59 -0300

    refactor: Formatted : constants, main, board

--- projects/02-tic-tac/src/logic/board.js
+++ projects/02-tic-tac/src/logic/board.js
@@ -1,26 +1,25 @@
-import {WINNER_COMBOS} from "../constants"
+import { WINNER_COMBOS } from '../constants'
 
 export const checkWinnerFrom = (boardToCheck) => {
-// Revisamos todas las combinaciones
-//Para ver si ganó X o O
- for (const combo of WINNER_COMBOS) {
-     const [a, b, c] = combo
-     if (
-       boardToCheck[a] &&
-       boardToCheck[a] === boardToCheck[b] &&
-       boardToCheck[a] === boardToCheck[c]
-     ) {
-       return boardToCheck[a]
-     }
-   }
-   // si no hay ganador
-   return null
+  // Revisamos todas las combinaciones
+  // Para ver si ganó X o O
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo
+    if (
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  // si no hay ganador
+  return null
 }
 
 export const checkEndGame = (newBoard) => {
-    //Revisamos si hay empate
-    //Si no hay mas espacios vacios
-    //en el tablero
-    return newBoard.every((square) => square !== null)
-
+  // Revisamos si hay empate
+  // Si no hay mas espacios vacios
+  // en el tablero
+  return newBoard.every((square) => square !== null)
 }
