08cb7be4ef05b6634c49b6b3d3f4971428eb5a17
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-19 18:37:56 -0300

    feat: add pointer style

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -7,10 +7,24 @@ function App () {
   }, [enabled])
   return (
     <main>
-      <h3>Projecto 3</h3>
-      <button onClick={() => setEnabled(!enabled)}>
-        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
-      </button>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: 'translate(0px, 0px)'
+      }}
+      >
+        <h3>Projecto 3</h3>
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+        </button>
+      </div>
     </main>
   )
 }
