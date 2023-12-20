fe5114bf47e631cfeedcf53a0a0d7e92c66bf4c0
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-20 11:07:08 -0300

    refactor: App

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,52 +1,71 @@
 import { useEffect, useState } from 'react'
 
-function App () {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
   // pointer move
   useEffect(() => {
     console.log('efecto', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup:
+    // -> Cuando el componente se monta
+    // -> Cuando cambian las dependencias antes de ejecutar
+    // el efecto de nuevo
+    return () => { // cleanup method
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
+  // [] -> solo se ejecuta una vez cuando se monta el componente
+  // [enabled] -> se ejecuta cuando cambia enabled y cuando se monta el componente
+  // undefined -> se ejecuta cada vez que se renderiza el componente
 
-  const handleMove = (event) => {
-    const { clientX, clientY } = event
-    setPosition({ x: clientX, y: clientY })
-  }
-  if (enabled) {
-    window.addEventListener('pointermove', handleMove)
-  }
+  // change body className
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
 
-  // cleanup:
-  // -> Cuando el componente se monta
-  // -> Cuando cambian las dependencias antes de ejecutar
-  // el efecto de nuevo
-  return () => { // cleanup method
-    console.log('cleanup')
-    window.removeEventListener('pointermove', handleMove)
-  }
-}, [enabled])
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
 
   return (
     <main>
       <div style={{
         position: 'absolute',
-        backgroundColor: '#09f',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: 'translate(0px, 0px)'
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
       }}
       >
-        <h3>Projecto 3</h3>
         <button onClick={() => setEnabled(!enabled)}>
           {enabled ? 'Desactivar' : 'Activar'} seguir puntero
         </button>
       </div>
+    </main>
+  )
+}
+
+function App () {
+  return (
+    <main>
+      <FollowMouse />
     </main>
   )
 }
