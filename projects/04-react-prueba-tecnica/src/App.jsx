f2cd15ddb4837bd5cce428fce60fe902901fe2c7
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-26 11:07:47 -0300

    feat: add const endpoint

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react'
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
 export function App () {
   const [fact, setFact] = useState('lorem ipsum cat fact')
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
+      .then(data => setFact(data.fact))
   }, [])
   return (
     <main>
