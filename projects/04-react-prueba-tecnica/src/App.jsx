07f640613ef7941ef846267198fb6cd71ac42ffb
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-26 11:19:54 -0300

    feat: update app

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 
 export function App () {
-  const [fact, setFact] = useState('lorem ipsum cat fact')
+  const [fact, setFact] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -13,7 +13,7 @@ export function App () {
   return (
     <main>
       <h1>App de Gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
     </main>
   )
 }
