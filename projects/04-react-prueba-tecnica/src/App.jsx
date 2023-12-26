eddf5fb414661fbfa361f2f0dbe6fb633945b871
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-25 22:59:06 -0300

    feat: add data fetching

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,7 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export function App () {
   const [fact, setFact] = useState('lorem ipsum cat fact')
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then(res => res.json())
+  }, [])
   return (
     <main>
       <h1>App de Gatitos</h1>
