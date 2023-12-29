de385cbdb964c4b2b96e08412ec8727772a9b6f9
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-29 11:35:19 -0300

    feat: update hook: useCatImage

--- projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
+++ projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
@@ -1,1 +1,23 @@
 import { useState, useEffect } from 'react'
+
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
+export function useCatImage ({ fact }) {
+  const [imageUrl, setImageUrl] = useState()
+
+  // Para recuperar la imagen cada vez que tengamos una cita
+  useEffect(() => {
+    if (!fact) return
+
+    const threeFisrtWords = fact.split(' ', 3).join(' ')
+
+    fetch(`https://cataas.com/cat/says/${threeFisrtWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(url)
+      })
+  }, [fact])
+
+  return { imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}` }
+}
