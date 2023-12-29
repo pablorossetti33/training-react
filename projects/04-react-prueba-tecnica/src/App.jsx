2ed46ca5eacb67101ac98509e1c11e4747b30d3a
Pablo Rossetti <prensap.ross@gmail.com>
2023-12-29 11:48:20 -0300

    feat: update App

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,25 +1,23 @@
-import { useEffect, useState } from 'react'
-
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-// const CAT_ENDPOINT_IMAGE_URL:
+import './App.css'
+import { useCatImage } from './hooks/useCatImage'
+import { useCatFact } from './hooks/useCatFact'
 
 export function App () {
-  const [fact, setFact] = useState()
+  const { fact, refreshFact } = useCatFact()
+  const { imageUrl } = useCatImage({ fact })
 
-  useEffect(() => {
-    fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(data.fact)
+  const handleClick = async () => {
+    refreshFact()
+  }
 
-        const firstWord = fact.split(' ').slice(0, 3).join(' ')
-      })
-  }, [])
   return (
     <main>
       <h1>App de Gatitos</h1>
+
+      <button onClick={handleClick}>Get new fact</button>
+
       {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first rhee words for ${fact}`} />}
     </main>
   )
 }
