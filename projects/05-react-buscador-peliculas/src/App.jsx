00b7e9ef2dc95b22220983979e8f748e2bd077c7
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-05 11:41:32 -0300

    feat: update App.jsx

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,10 @@
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App () {
+  const movies = responseMovies.Search
+  const hasMovies = movies?.length > 0
   return (
     <div className='page'>
       <header>
@@ -11,7 +15,24 @@ function App () {
         </form>
       </header>
       <main>
-        All of movies
+        {
+          hasMovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+          ) : (
+            <p>No se encontraron Peliculas</p>
+          )
+        }
       </main>
     </div>
   )
