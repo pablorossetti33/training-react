d3f130e450ac854a7cc34f7a383716cd9db7e55b
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-10 19:52:01 -0300

    feat: add hook useRef

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,20 +1,27 @@
 import './App.css'
+import { useRef } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App () {
-  const { movies: mappedMovies } = useMovies()
+  const { movies } = useMovies()
+  const inputRef = useRef()
+
+  const handleClick = () => {
+    const value = inputRef.current.value
+    console.log(value)
+  }
   return (
     <div className='page'>
       <header>
         <h1>Movie Search.</h1>
         <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix..' />
-          <button type='submit'>Search</button>
+          <input ref={inputRef} placeholder='Avengers, Star Wars, The Matrix..' />
+          <button onClick={handleClick} type='submit'>Search</button>
         </form>
       </header>
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   )
