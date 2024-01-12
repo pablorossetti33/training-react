a78f9211303ebc34baede8a6b80a3c8169a5e55d
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-12 11:54:29 -0300

    feat: change hooks

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,13 +1,36 @@
-import responseMovies from '../mocks/with-results.json'
-import withoutResults from '../mocks/no-results.json'
+import { useRef, useState, useMemo, useCallBack } from 'react'
+import { searchMovies } from '../services/movies'
 
-export function useMovies () {
-  const movies = responseMovies.Search
-  const mappedMovies = movies?.map(movie => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster
-  }))
-  return { movies: mappedMovies }
+export function useMovies ({ search, sort }) {
+  const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  // El error no se usa pero se puede implementar
+  const [, setError] = useState(null)
+  const previousSearch = useRef(search)
+
+  const getMovies = useCallBack(async ({ search }) => {
+    if (search === previousSearch.current) return
+    // search es ''
+
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError(e.message)
+    } finally {
+      // tanto en el try como en el catch
+      setLoading(false)
+    }
+  }, [])
+
+  const sortedMovies = useMemo(() => {
+    return sort
+      ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
+      : movies
+  }, [sort, movies])
+
+  return { movies: sortedMovies, getMovies, loading }
 }
