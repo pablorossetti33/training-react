fe07615d35ad6cfc9e126cffef56465277c06504
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-22 21:10:08 -0300

    feat: add component products

---
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,20 @@
+import './Products.css'
+import { AddToCartIcon } from './Icons'
+
+export function Products ({ products }) {
+    return (
+        <main>
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                    <img
+                src={product.thumbnail}
+                alt={product.title}
+              />
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+
+}
