550f25731a01242fe8ae5460cd21aa73f4f79919
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-22 21:13:15 -0300

    feat: update products

--- projects/06-shopping-cart/src/components/Products.jsx
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -2,19 +2,18 @@ import './Products.css'
 import { AddToCartIcon } from './Icons'
 
 export function Products ({ products }) {
-    return (
-        <main>
-            <ul>
-                {products.map(product => (
-                    <li key={product.id}>
-                    <img
-                src={product.thumbnail}
-                alt={product.title}
-              />
-                    </li>
-                ))}
-            </ul>
-        </main>
-    )
-
+  return (
+    <main>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
