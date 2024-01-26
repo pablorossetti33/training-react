d0c17b96e460bcb5958345065bafe5e1bcbdf99e
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-26 12:24:14 -0300

    feat: add useState App

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,6 +1,8 @@
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products'
+import { useState } from 'react'
 function App () {
+  const [products] = useState(initialProducts)
   return (
     <Products products={products} />
   )
