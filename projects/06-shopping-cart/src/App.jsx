45aecc36bac3b2efa2e40d3f45057f390a9b59f0
Pablo Rossetti <prensap.ross@gmail.com>
2024-01-29 12:01:57 -0300

    feat: app: add filters states

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,6 +3,22 @@ import { Products } from './components/Products'
 import { useState } from 'react'
 function App () {
   const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category:'all',
+    minPrice: 0,
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && 
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
   return (
     <Products products={products} />
   )
