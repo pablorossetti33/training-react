bd5e1528554fe396666b0b6ad526e8c978e1f926
Pablo Rossetti <prensap.ross@gmail.com>
2024-02-15 12:51:47 -0300

    feat: config

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -3,6 +3,7 @@ import { Products } from './components/Products'
 import { useState } from 'react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
+import { IS_DEVELOPMENT } from './config'
 
 function useFilters () {
   const [filters, setFilters] = useState({
@@ -34,7 +35,7 @@ function App () {
     <>
       <Header changeFilters={setFilters} />
       <Products products={filteredProducts} />
-      <Footer filters={filters} />
+      {IS_DEVELOPMENT && <Footer filters={filters} />}
     </>
   )
 }
