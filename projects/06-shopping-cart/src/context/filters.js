2a4f871939c36c720fd61c08bd54bfa6f3135f21
Pablo Rossetti <prensap.ross@gmail.com>
2024-02-21 10:38:41 -0300

    feat: add contect and provider

--- projects/06-shopping-cart/src/context/filters.js
+++ projects/06-shopping-cart/src/context/filters.js
@@ -2,3 +2,16 @@ import { createContext } from 'react'
 
 // Crear el context
 export const FiltersContext = createContext()
+
+// Crear el provider, para proveer el contexto.
+export function FilterProvider ({ children }) {
+  return (
+    <FiltersContext.Provider value={{
+      category: 'all',
+      minPrice: 0
+    }}
+    >
+      {children}
+    </FiltersContext.Provider>
+  )
+}
